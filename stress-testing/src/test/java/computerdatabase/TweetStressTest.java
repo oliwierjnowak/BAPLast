c7b27f21f110839403d2ed7c790345b09d1e7f9e
Oliwier Nowak <NOW20328@spengergasse.at>
2024-04-15 22:01:01 +0200

    perf testing

--- stress-testing/src/test/java/computerdatabase/TweetStressTest.java
+++ stress-testing/src/test/java/computerdatabase/TweetStressTest.java
@@ -1,2 +1,45 @@
-package computerdatabase;public class TweetStressTest {
+package computerdatabase;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+public class TweetStressTest extends Simulation {
+    private HttpProtocolBuilder httpTweet = http.baseUrl("http://localhost:8080")
+            .acceptHeader("application/json")
+            .contentTypeHeader("application/json");
+
+    private static ChainBuilder createTweet =
+            exec(http("create some tweet")
+                    .post("http://localhost:8080/tweet")
+                    .body(ElFileBody("bodies/tweet.json")).asJson());
+
+    private static ChainBuilder getAllTweets =
+            exec(http("getAllTweets")
+                    .get("http://localhost:8080/tweet"));
+    private static ChainBuilder getSingleTweet =
+            exec(http("create many")
+                    .get("http://localhost:8080/tweet/cernil"));
+
+    private ScenarioBuilder setupTweets =scenario("setup Tweet").exec(createTweet);
+
+
+
+    private ScenarioBuilder tweetsMany = scenario("get list ")
+            .exec(getAllTweets)
+            .pause(2)
+            .exec(getSingleTweet);
+
+    {
+        setUp(
+
+
+                                setupTweets.injectOpen(atOnceUsers(1))
+                                        .protocols(httpTweet).andThen(tweetsMany.injectOpen(atOnceUsers(10)))
+
+
+
+        );
+    }
 }
